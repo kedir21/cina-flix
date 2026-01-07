aac37aac81701c0a4d0f2ddc61486c5b0c107597
kedir21 <kedir.nasir.mohammed@gmail.com>
2026-01-07 20:50:33 +0300

    first commit

---
+++ android/app/src/main/java/com/cinestream/tv/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cinestream.tv;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cinestream.tv',
+  appName: 'cinestream-tv',
+  webDir: 'dist'
+};
+
+export default config;
